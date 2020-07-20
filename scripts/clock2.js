c51f0b26c0f6c0fdc30d0d6249e0d0d25d06709f
Evgeniy Lodygin <evgeniy.lodygin@gmail.com>
2020-07-20 12:40:33 +0300

    Function developed
    Everything works well, but there are several problems:
    1) Each second we have all our data updated - dd/hh/mm/ss, but at every second we only need to change secs value and every 1 minute, minutes value and so on...
    2) When we would be close to the season change date, which is called as "this.finalDate" in our Class, we will need to reload the page for everything works correctly, because we need to recalculate "month" var in initHeadingAndFinalDate() method. Better think about how to do this process work automatically.
    3) There is a glitch while texting DD data in our digits <li> in html.  Although it calculates as first parameter in getTime() and it is called in init() method as first parameter too it is shown at our page in the last moment.

---
+++ scripts/clock2.js
@@ -0,0 +1,62 @@
+class Clock{
+	constructor(id){
+		this.clock = $(id);
+		this.heading = $("h1");
+		this.exactDays = 0;
+		this.exactHours = 0;
+		this.exactMinutes = 0;
+		this.exactSeconds = 0;
+		this.finalDate = null;
+		this.initHeadingAndFinalDate();
+		this.init();
+		this.update();
+	}
+	initHeadingAndFinalDate(){
+		const currentDay = new Date()
+		,month = currentDay.getMonth()
+		;
+		if (month == 11 ||month == 0||month == 1){
+			this.heading.text(`Winter ends in:`);
+			this.finalDate = new Date(2021,2,1);
+		}
+		else if (month == 2 ||month == 3||month == 4){
+			this.heading.text(`Spring ends in:`);
+			this.finalDate = new Date(2021,5,1);
+		}
+		else if (month == 5 ||month == 6||month == 7){
+			this.heading.text(`Summer ends in:`);
+			this.finalDate = new Date(2020,8,1);
+		}
+		else{
+			this.heading.text(`Autumn ends in:`);
+			this.finalDate = new Date(2020,11,1);
+		}
+	}
+
+	init(){
+		this.getTime(".days", this.exactDays);
+		this.getTime(".hours", this.exactHours);
+		this.getTime(".min", this.exactMinutes);
+		this.getTime(".sec", this.exactSeconds);
+	}
+	update(){
+		setInterval(this.init.bind(this),1000);
+	}
+	getTime(cssSelector, dateComponent){
+		const now = new Date();
+
+		const days = (this.finalDate - now)/(1000*60*60*24);
+			this.exactDays = Math.floor(days);
+		const hours = (days - this.exactDays)*24
+			this.exactHours = Math.floor(hours);
+		const minutes = (hours - this.exactHours)*60
+			this.exactMinutes = Math.floor(minutes);
+		const seconds = (minutes - this.exactMinutes)*60
+			this.exactSeconds = Math.floor(seconds);
+		const timePlace = this.clock.find(cssSelector)
+			, timeData	= dateComponent;
+			;
+		timePlace.text(timeData < 10 ? `0${timeData}` : timeData);
+	}
+}
+let clock1 = new Clock("#clock1");
